f2f31e65ed9e46255d0254a967641938b200175c
Dim Entelis <dentelis@gmail.com>
2024-05-22 19:45:07 +0200

    rector config

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -14,12 +13,15 @@ return RectorConfig::configure()
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
-        codingStyle:true,
+        codingStyle: true,
         typeDeclarations: true,
-        privatization:true,
+        privatization: true,
         naming: false, //change variable names to class names
         instanceOf: true, //unclear
         earlyReturn: false, //need review
         strictBooleans: true,
         carbon: false, //https://carbon.nesbot.com/docs/
-    );
+    )->withSkip([
+        \Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class, //lower code readability
+        \Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector::class, //breaks code
+    ]);;
